3ae169b039e2c9d39f33d532b735412e6cc110b0
hwanwook100510 <hwanwook100510@gmail.com>
2026-08-25 15:40:41 +0900

    edit ex4.py add ex5.py

--- 01_datatype/ex4.py
+++ 01_datatype/ex4.py
@@ -16,11 +16,9 @@ You need Python
 
 print(multiline)
 
-
 def func():
     """이 함수는 테스트용입니다."""
     pass
-
 
 print(func.__doc__)
 
@@ -31,4 +29,27 @@ print("Hello" + "Python")
 print("Hello" * 10)
 print("-" * 100)
 
-print("Hello" + 10)
+# print("Hello" + 10)
+print("Hello" + str(10))
+
+print(int("10") + int("2"))
+
+# 문자열 포맷팅 (f-string)
+name = "pororo"
+age = 23
+
+print(f"이름 : {name}, 나이 : {age}"살)
+print(f"내년 나이: {age + 1}살")
+print(f"{name.upper()}님 안녕하세요")
+
+pi = 3.141592
+
+print(f"{pi:.3f}")
+print(f"{pi:.0f}")
+
+num = 123456789
+
+print(f"{num:,}")
+print(f"{num:15,d}")
+print(f"{num:<15d}")
+print(f"{num:015,d}")
